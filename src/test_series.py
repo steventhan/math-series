8e30cacc505d512908d117e7d48b8451412a9e48
Steven Than <steventhan11@gmail.com>
2016-08-09 19:43:40 -0700

    parametrized the test for lucas function, cover negitve n value, base case values, and other values

--- src/test_series.py
+++ src/test_series.py
@@ -19,6 +19,20 @@ def test_fibonacci(n, result):
     assert fibonacci(n) == result
 
 
-def test_lucas():
+LUCAS_TABLE = [
+    (-1, False),
+    (0, 2),
+    (1, 1),
+    (2, 3),
+    (3, 4),
+    (5, 7),
+    (6, 11),
+    (7, 18),
+    (8, 29)
+]
+
+
+@pytest.mark.parametrize('n, result', LUCAS_TABLE)
+def test_lucas(n, result):
     from series import lucas
     assert lucas(6) == 18
